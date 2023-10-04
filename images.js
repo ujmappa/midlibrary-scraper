a726cd8f86a5c78fc2f2f8b1d82da12008003c5c
ujmappa <79494755+ujmappa@users.noreply.github.com>
2023-10-04 23:28:34 +0200

    Create directory if not exists

--- images.js
+++ images.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
-const fs = require('node:fs/promises');
+const fs = require('node:fs');
 
 const settings = require('./settings.json');
 const database = require('./database.json');
 
 const Scraper = require('./sources/scrapers');
 
+
+if (!fs.existsSync('./images')) { 
+    fs.mkdirSync('./images', 0o744);
+}
 
 (async () => {
     const scraper = new Scraper(settings);  
