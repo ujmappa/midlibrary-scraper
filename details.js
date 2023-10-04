e2ecd768c93346bec48fc79c69fde861faf3c281
ujmappa <79494755+ujmappa@users.noreply.github.com>
2023-10-04 23:29:50 +0200

    Create directory if not exists

--- details.js
+++ details.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const fs = require('node:fs/promises');
+const fs = require('node:fs');
 
 const settings = require('./settings.json');
 const database = require('./database.json');
@@ -8,10 +8,14 @@ const database = require('./database.json');
 const Scraper = require('./sources/scrapers');
 
 
+if (!fs.existsSync('./assets')) { 
+    fs.mkdirSync('./assets', 0o744);
+}
+
 (async () => {
     const scraper = new Scraper(settings);
     await scraper.downloadAssets(database, './assets');
 
     const json = JSON.stringify(database, null, '\t');
-    await fs.writeFile('./database.json', json);
+    fs.writeFileSync('./database.json', json);
 })();
